e3740c5969522ad00699a8ad6ea4aa08fa19801e
Bahram Torani <bahram4531@yahoo.com>
2023-01-28 23:03:49 +0100

    Update Task2.kt

--- src/main/kotlin/lms_126/Task2.kt
+++ src/main/kotlin/lms_126/Task2.kt
@@ -3,9 +3,9 @@ package lms_126
 fun task2(): String {
 
     return """
-        INPUT base of rectandle
+        INPUT base of rectangle 
         INPUT height of rectangle
         Calculate are by multiplying base time height
         OUTPUT area of rectangle
-        """
+     """
 }
