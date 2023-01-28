5d6de9c84661e3cfa809a7373d34fcb23f1a9b5d
Bahram Torani <bahram4531@yahoo.com>
2023-01-28 17:28:33 +0100

    Solution Task2

--- src/main/kotlin/lms_126/Task2.kt
+++ src/main/kotlin/lms_126/Task2.kt
@@ -2,5 +2,10 @@ package lms_126
 
 fun task2(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        INPUT base of rectandle
+        INPUT height of rectangle
+        Calculate are by multiplying base time height
+        OUTPUT area of rectangle
+        """
 }
