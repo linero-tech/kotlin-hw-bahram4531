a933b658325d43595624468436b8ae42181bb875
Bahram Torani <bahram4531@yahoo.com>
2023-02-04 00:52:26 +0100

    Update Task3.kt

--- src/main/kotlin/lms_126/Task3.kt
+++ src/main/kotlin/lms_126/Task3.kt
@@ -1,15 +1,10 @@
 package lms_126
 
-fun task3(): String
-{
-    return """
-       INPUT Height of triangle
-       OUTPUT area of triangle 
-       
-       Calculator area = 0 . 5 * base * height
-       OUTPUT area of triangle
-   """
-}
-
-
-
+fun task3(): String {
+    return
+    """
+      INPUT Height of triangle
+      OUTPUT area of triangle 
+      Calculator area = 0 . 5 * base * height
+      OUTPUT area of triangle
+    """
