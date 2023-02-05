bb5a5d04829f7f607b5096c43c0c4b0461a1e629
Bahram Torani <bahram4531@yahoo.com>
2023-02-05 23:31:32 +0100

    Update Task4.kt

--- src/main/kotlin/lms_126/Task4.kt
+++ src/main/kotlin/lms_126/Task4.kt
@@ -3,8 +3,14 @@ package lms_126
 fun task4(): String {
 
     return
-    val x = "1"
-    val y = "2"
-    val moduloResult = "1 % 2"
-    println("moduloResult")
+    fun main() {
+        val x = 7
+        val y = 11
+
+        if (x > y) {
+            println(x)
+        } else {
+            println(y)
+        }
+    }
 }
