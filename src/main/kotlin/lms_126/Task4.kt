618ee79b9f15fb231a7c65642f0982e01b279b43
Bahram Torani <bahram4531@yahoo.com>
2023-02-05 03:19:19 +0100

    Update Task4.kt

--- src/main/kotlin/lms_126/Task4.kt
+++ src/main/kotlin/lms_126/Task4.kt
@@ -1,10 +1,9 @@
 package lms_126
 
 fun task4(): String {
-
-    return "
-    val x = 1
-    val y = 2
-    val moduloResult = 1 % 2
-    println(moduloResult)
-}  " 
+    return
+    val x = "1"
+    val y = "2"
+    val moduloResult = "1 % 2"
+    println("moduloResult")
+}
