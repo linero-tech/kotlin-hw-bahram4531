3c2d9492e9e697898d9ba5517d97cbad27f993ae
Bahram Torani <bahram4531@yahoo.com>
2023-02-06 17:32:05 +0100

    Update Task11.kt

--- src/main/kotlin/lms_126/Task11.kt
+++ src/main/kotlin/lms_126/Task11.kt
@@ -2,5 +2,12 @@ package lms_126
 
 fun task11(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+      INPUT: temperature value with unit ("C or F")
+      INPUT: the unit of the input temperature
+      IF: the unit is C, convert it to Fahrenheit using the formula: value in C * 9 / 5 + 32
+      IF: the unit is F, convert it to Celsius using the formula: value in F - 32 * 5 / 9
+      OUTPUT: the result with the new unit ("F or C")
+      OUTPUT: transaction
+    """
 }
