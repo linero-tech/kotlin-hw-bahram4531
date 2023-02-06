5dc50cc6db4da2d3eb2b361edebbc18c4684e6c6
Bahram Torani <bahram4531@yahoo.com>
2023-02-06 17:04:43 +0100

    Update Task10.kt

--- src/main/kotlin/lms_126/Task10.kt
+++ src/main/kotlin/lms_126/Task10.kt
@@ -2,5 +2,12 @@ package lms_126
 
 fun task10(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+   INPUT: email address
+   INPUT: password
+   IF: password length is greater than 6 characters
+   IF: yes, store email and password in database
+   IF: no, display error message "Password must be at least 6 characters long
+   OUTPUT transaction     
+    """
 }
