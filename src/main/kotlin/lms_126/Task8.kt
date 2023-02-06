8d8cc8eca42e7f31222952cc1582f9faa7dbbd9c
Bahram Torani <bahram4531@yahoo.com>
2023-02-06 16:12:51 +0100

    Update Task8.kt

--- src/main/kotlin/lms_126/Task8.kt
+++ src/main/kotlin/lms_126/Task8.kt
@@ -1,6 +1,14 @@
 package lms_126
 
 fun task8(): String {
-
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+    IF X is divisible by 3 and 5:
+    print "FooBar"
+    ELSE if X is divisible by 3:
+    println("Foo")
+    ELSE if X is divisible by 5:
+    print "Bar"
+    ELSE:
+    print "Qix    
+    """
 }
