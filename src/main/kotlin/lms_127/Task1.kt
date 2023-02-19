ae67ff4eeab49cc16f94ad1367e2168bc94f5d4d
Bahram Torani <bahram4531@yahoo.com>
2023-02-19 22:22:35 +0100

    Update Task1.kt

--- src/main/kotlin/lms_127/Task1.kt
+++ src/main/kotlin/lms_127/Task1.kt
@@ -1,9 +1,11 @@
 package lms_127
 
 fun task1() {
-return
-    val name = "Bahram"
-    val capital = "Stockholm"
-    val currency = "Swedish krona"
+
+val name = "Bahram"
+val capital = "Stockholm"
+val currency = "Swedish krona"
     println("Name: $name\nCapital: $capital\nCurrency: $currency")
+
+
 }
