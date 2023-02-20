f3ae04b7747648ea267ec7978e333a16f1d6dc83
Bahram Torani <bahram4531@yahoo.com>
2023-02-20 09:37:09 +0100

    Update Task2.kt

--- src/main/kotlin/lms_126/Task2.kt
+++ src/main/kotlin/lms_126/Task2.kt
@@ -2,7 +2,7 @@ package lms_126
 
 fun task2(): String {
 
-     return """
+     return """ 
           INPUT base of rectangle
           INPUT height of rectangle
           Calculate area by multiplying base times height
