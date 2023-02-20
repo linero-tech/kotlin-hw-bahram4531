3f56732d26ee890895a52745f214229554c533be
Bahram Torani <bahram4531@yahoo.com>
2023-02-21 00:37:45 +0100

    Update Task1.kt

--- src/main/kotlin/lms_127/Task1.kt
+++ src/main/kotlin/lms_127/Task1.kt
@@ -5,9 +5,9 @@ fun task1() {
     val name = "Bahram"
     val capital = "Stockholm"
     val currency = "Swedish krona"
-
-    val completeName == name +" "+ capital + " " + currency // string concatenation
     val sentence = "$name $capital $currency" // string templates
-    println(completeName)
     println(sentence)
     }
+fun main() {
+    task1()
+}
