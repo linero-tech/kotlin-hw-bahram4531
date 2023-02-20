bae50a21ee82c7355fe788cb0b51b110aa1717ae
Bahram Torani <bahram4531@yahoo.com>
2023-02-20 08:31:41 +0100

    102 task3

--- src/main/kotlin/lms_127/Task3.kt
+++ src/main/kotlin/lms_127/Task3.kt
@@ -1,4 +1,4 @@
 package lms_127
 
-fun task3(radius: Double): Double {
+ fun task3(radius: Double): Double {
     return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
