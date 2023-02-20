ca7d4b0c861fd3ea0014e16712562e9a896682b6
Bahram Torani <bahram4531@yahoo.com>
2023-02-20 09:37:45 +0100

    Update Task3.kt

--- src/main/kotlin/lms_126/Task3.kt
+++ src/main/kotlin/lms_126/Task3.kt
@@ -2,7 +2,7 @@ package lms_126
 
 fun task3(): String {
 
-     return """
+     return """ 
      INPUT Height of triangle
      OUTPUT area of triangle 
      Calculator area = 0.5 * base * height
