c2e8091e92b821ef764856f761065de7cc45f5b7
Bahram Torani <bahram4531@yahoo.com>
2023-02-23 20:58:35 +0100

    Update Task1.kt

--- src/main/kotlin/lms_127/Task1.kt
+++ src/main/kotlin/lms_127/Task1.kt
@@ -1,22 +1,13 @@
 package lms_127
 
-fun task1() {
+fun main task1() {
 
     val country = "Sweden"
     val capital = "Stockholm"
     val currency = "Swedish krona"
-
-    val completeName = country + " " + capital + " " + currency // string concatenation
     val sentence = "$country $capital $currency" // string templates
-    println(completeName)
-    println(completeName)
     println(sentence)
 
     val result
-    return result
-}
 
-}
-    fun main() result
-    task1()
 }
