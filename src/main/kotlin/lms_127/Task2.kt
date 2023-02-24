3d1fc92725aaa2c428948b087579d599b0251c10
Bahram Torani <bahram4531@yahoo.com>
2023-02-24 14:29:46 +0100

    Update Task2.kt

--- src/main/kotlin/lms_127/Task2.kt
+++ src/main/kotlin/lms_127/Task2.kt
@@ -5,7 +5,7 @@ fun task2() {
     val name = "Bahram"
     val country = "Sweden"
     val month = "July"
-    println("Hello $name, you are from $country and you were born in $month.")
+    println("Hello $name, you are from $country and you were born in $month")
 }
 
 fun main() {
