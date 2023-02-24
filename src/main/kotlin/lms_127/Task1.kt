9c6ee1a2a8f34a46380bc3baffda992f4040814a
Bahram Torani <bahram4531@yahoo.com>
2023-02-24 03:20:56 +0100

    Update Task1.kt

--- src/main/kotlin/lms_127/Task1.kt
+++ src/main/kotlin/lms_127/Task1.kt
@@ -4,9 +4,10 @@ fun task1() {
     val country = "Sweden"
     val capital = "Stockholm"
     val currency = "Swedish krona"
-    val result = "$country $capital $currency"
+
+    println("$country $capital $currency")
 }
-fun main () {
-    val result = task1()
-    println(result)
+
+fun main() {
+
 }
