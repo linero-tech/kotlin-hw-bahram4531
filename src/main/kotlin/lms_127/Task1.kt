7c0f7071c5f804de1a722374cc2960edac751ee5
Bahram Torani <bahram4531@yahoo.com>
2023-02-26 18:22:49 +0100

    Update Task1.kt

--- src/main/kotlin/lms_127/Task1.kt
+++ src/main/kotlin/lms_127/Task1.kt
@@ -4,9 +4,9 @@ fun task1() {
     val country = "Sweden"
     val capital = "Stockholm"
     val currency = "Swedish SEK"
-    println("COUNTRY NAME : $country")
-    println("CAPITAL NAME : $capital")
-    println("CURRENCY NAME : $currency")
+    println("Country: $country")
+    println("Capital: $capital")
+    println("Currency: $currency")
 }
 
 fun main () {
