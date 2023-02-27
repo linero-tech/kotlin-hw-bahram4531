3de48351fa9b0b26bd1192be058c4bd96ace6d83
Bahram Torani <bahram4531@yahoo.com>
2023-02-27 02:11:28 +0100

    Update Task5.kt

--- src/main/kotlin/lms_127/Task5.kt
+++ src/main/kotlin/lms_127/Task5.kt
@@ -4,9 +4,14 @@ fun task5(valueForA: Int, valueForB: Int): Pair<Int, Int> {
     // Do not change or delete these variables
     var a = valueForA
     var b = valueForB
-
-    TODO("Your solutions goes in here.")
-
+    a -= b
+    b += a
+    a = b - a
     // Do not erase or change this statement
     return Pair(a, b)
 }
+fun main() {
+    val answer = task5(1,2)
+    println(answer)
+}
+
