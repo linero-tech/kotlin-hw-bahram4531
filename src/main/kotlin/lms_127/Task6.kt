5a2d95d6b19826e6a01ca1bc22a7b6811b4d72d4
Bahram Torani <bahram4531@yahoo.com>
2023-02-28 16:02:09 +0100

    Update Task6.kt

--- src/main/kotlin/lms_127/Task6.kt
+++ src/main/kotlin/lms_127/Task6.kt
@@ -2,5 +2,11 @@ package lms_127
 
 fun task6(base1: Double, base2: Double, height: Double): Double {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val :"base + base == x "
+    }
+    fun main()  {
+        println(task6(base1 = 10.0, base2 = 20.0, height = 1.0))
+        println(task6(base1 = 20.0, base2 = 40.0, height = 2.0))
+    }
+    return
 }
