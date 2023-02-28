a26f910e7726d2249b70578a2b24ef5b0271d59a
Bahram Torani <bahram4531@yahoo.com>
2023-02-28 16:12:57 +0100

    Update Task6.kt

--- src/main/kotlin/lms_127/Task6.kt
+++ src/main/kotlin/lms_127/Task6.kt
@@ -1,12 +1,11 @@
 package lms_127
 
-fun task6(base1: Double, base2: Double, height: Double): Double {
-
-    val :"base + base == x "
-    }
-    fun main()  {
-        println(task6(base1 = 10.0, base2 = 20.0, height = 1.0))
-        println(task6(base1 = 20.0, base2 = 40.0, height = 2.0))
-    }
-    return
+fun task6(base1: Double, base2: Double, height: Double): Double
+{
+    return (base1 + base2) * 0.5 * height
 }
+fun main() {
+    val answer = task6(10.0 , 20.0 , 1.0)
+    println(answer)
+}
+
