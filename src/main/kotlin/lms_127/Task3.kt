7bd7172f5c77afee6bcf0bc0601f289b87f928c8
Bahram Torani <bahram4531@yahoo.com>
2023-03-05 23:39:05 +0100

    Update Task3.kt

--- src/main/kotlin/lms_127/Task3.kt
+++ src/main/kotlin/lms_127/Task3.kt
@@ -1,13 +1,11 @@
 package lms_127
 
  fun task3(radius: Double): Double {
-     val radius = 2.0
-     val result = 2.0 * radius * 3.1416
+     val result = 2 * Math.PI * radius
 
 
      return result
  }
 fun main() {
-    val result = task3(radius = 2.0)
-    println(result)
+  task3(2.0)
 }
