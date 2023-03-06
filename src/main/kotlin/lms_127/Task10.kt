2af854375f6a34b7e2ffb0a5869c1ae006fa4178
Bahram Torani <bahram4531@yahoo.com>
2023-03-06 14:50:47 +0100

    Update Task10.kt

--- src/main/kotlin/lms_127/Task10.kt
+++ src/main/kotlin/lms_127/Task10.kt
@@ -1,24 +1,21 @@
 package lms_127
 
-fun task10_1(assessments: String): Int {
-
-    val result = assessments.length
-    return result    }
+fun task10_1(assessments: String): Int
+{
+    return assessments.length
 }
 
-fun task10_2(assessments: String): Char {
-
-    val answer = task10_2("LMFHMF")
-    println(answer)
+fun task10_2(assessments: String): Char
+{
+    return assessments[3]
 }
 
-fun task10_3(assessments: String): Char {
-
-    return assessments[assessments.length/2]
+fun task10_3(assessments: String): Char
+{
+    return assessments.get(assessments.length / 2)
 }
 
-fun task10_4(assessments: String): String {
-
-    val result = assessments.substring(0,3)
-    return result
+fun task10_4(assessments: String): String
+{
+    return assessments.substring(0, 3)
 }
