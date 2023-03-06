1672de32c7b437fd8c85f4369b59581dcff4680f
Bahram Torani <bahram4531@yahoo.com>
2023-03-06 15:20:42 +0100

    Update Task1.kt

--- src/main/kotlin/lms_129/Task1.kt
+++ src/main/kotlin/lms_129/Task1.kt
@@ -1,6 +1,18 @@
 package lms_129
 
 fun task1(a: Int, b: Int): Int {
+    val a = 1
+    val b = 5
+    var result = 0
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
-}
+    if (a >= b) {
+        println("result is $result")
+    } else {
+        for (i in a..b) {
+            result += i
+        }
+        println("result is $result")
+        return result
+    }
+
+
