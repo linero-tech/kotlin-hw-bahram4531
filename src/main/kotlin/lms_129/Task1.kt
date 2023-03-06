be91da1ef1db1ae9ce10be58414d54d3eda056fe
Bahram Torani <bahram4531@yahoo.com>
2023-03-06 15:30:49 +0100

    Update Task1.kt

--- src/main/kotlin/lms_129/Task1.kt
+++ src/main/kotlin/lms_129/Task1.kt
@@ -1,18 +1,11 @@
 package lms_129
 
 fun task1(a: Int, b: Int): Int {
-    val a = 1
-    val b = 5
+
     var result = 0
 
-    if (a >= b) {
-        println("result is $result")
-    } else {
-        for (i in a..b) {
-            result += i
-        }
-        println("result is $result")
+    if (a >= b)
+    { return result }
+        else { for (i in a..b) { result += i }
         return result
     }
-
-
