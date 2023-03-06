487cfd93088f08cce006f9ac6868fdefa1dce921
Bahram Torani <bahram4531@yahoo.com>
2023-03-06 15:39:30 +0100

    Update Task1.kt

--- src/main/kotlin/lms_129/Task1.kt
+++ src/main/kotlin/lms_129/Task1.kt
@@ -5,7 +5,19 @@ fun task1(a: Int, b: Int): Int {
     var result = 0
 
     if (a >= b)
-    { return result }
-        else { for (i in a..b) { result += i }
+    {
         return result
     }
+    else
+    {
+        for (i in a..b)
+        {
+            result += i
+        }
+    }
+    return result
+}
+fun main() {
+    val result = task1(1,5)
+    println(result)
+}
