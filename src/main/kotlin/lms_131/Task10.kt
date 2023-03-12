ea05d5126e30a441f036bf414d4910de58dda553
Bahram Torani <bahram4531@yahoo.com>
2023-03-12 21:11:58 +0100

    Update Task10.kt

--- src/main/kotlin/lms_131/Task10.kt
+++ src/main/kotlin/lms_131/Task10.kt
@@ -1,6 +1,14 @@
 package lms_131
 
 fun task10(mapa: Map<Int, String>): Map<String, Int> {
+    val result = mutableMapOf<String, Int>()
+    for ((key, value) in mapa) {
+        result[value] = key
+    }
+return result
+}
+fun main(){
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val mapa = mapOf(1 to "A", 2 to "B", 3 to "C")
+println(task10(mapa))
 }
