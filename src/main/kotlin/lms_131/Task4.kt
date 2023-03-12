5b56489f3da28a3122173668fc43f14b06a0b6fa
Bahram Torani <bahram4531@yahoo.com>
2023-03-12 20:44:11 +0100

    Update Task4.kt

--- src/main/kotlin/lms_131/Task4.kt
+++ src/main/kotlin/lms_131/Task4.kt
@@ -2,5 +2,20 @@ package lms_131
 
 fun task4(mapa: Map<Int, Int>, value: Int): List<Int> {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+
+    val result = mutableListOf<Int>()
+    for ((key, v) in mapa) {
+        if (v == value) {
+            result.add(key)
+        }
+    }
+
+    return result
+
+}
+
+fun main() {
+   val mapa = mapOf(1 to 100, 2 to 100, 3 to 200)
+    val value = 100
+    println(task4(mapa, value))
 }
