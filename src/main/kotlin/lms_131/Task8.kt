bc46fe20d0f4f1aa7ce543ecaf6194c80875ea75
Bahram Torani <bahram4531@yahoo.com>
2023-03-12 21:02:27 +0100

    Update Task8.kt

--- src/main/kotlin/lms_131/Task8.kt
+++ src/main/kotlin/lms_131/Task8.kt
@@ -1,6 +1,22 @@
 package lms_131
 
 fun task8(grades: Map<String, List<Double>>): String {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
-}
+    var highestAverage = -1.0
+    var result = ""
+    for ((student, gradesList) in grades) {
+        var sum = 0.0
+        for (grade in gradesList) {
+            sum += grade
+        }
+        val average = sum / gradesList.size
+        if (average > highestAverage) {
+            highestAverage = average
+            result = student
+        }
+    }
+    return result
+    }
+ fun main() {
+     val grades = mapOf("Ana" to listOf(4.0, 4.5, 5.0), "John" to listOf(5.0, 5.0, 3.0), "Lise" to listOf(5.0, 5.0, 5.0))
+     println(task8(grades))
+ }
