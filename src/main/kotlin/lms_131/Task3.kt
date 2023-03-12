e9e3a0a58a99a12154a5f9b75588cb6002f14717
Bahram Torani <bahram4531@yahoo.com>
2023-03-12 20:38:29 +0100

    Update Task3.kt

--- src/main/kotlin/lms_131/Task3.kt
+++ src/main/kotlin/lms_131/Task3.kt
@@ -2,5 +2,20 @@ package lms_131
 
 fun task3(a: Map<Int, String>, b: Map<Int, String>): List<Int> {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+
+    val result = mutableListOf<Int>()
+    for (key in a.keys) {
+        if (b.containsKey(key)) {
+            result.add(key)
+        }
+    }
+
+    return result
+
+
+}
+fun main(){
+    val a = mapOf(1 to "A", 2 to "B", 3 to "C")
+    val b = mapOf(1 to "A", 2 to "B", 4 to "D")
+    println(task3(a,b))
 }
