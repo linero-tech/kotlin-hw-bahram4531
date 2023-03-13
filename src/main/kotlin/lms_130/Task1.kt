9d69cb49e3658e53397fc75a12ce4203d66dcd73
Bahram Torani <bahram4531@yahoo.com>
2023-03-13 15:42:54 +0100

    Update Task1.kt

--- src/main/kotlin/lms_130/Task1.kt
+++ src/main/kotlin/lms_130/Task1.kt
@@ -1,6 +1,21 @@
 package lms_130
 
+import kotlin.random.Random
+
 fun task1(items: List<Int>): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+
+    val result = if (items.isNotEmpty()) {
+
+        items[Random.nextInt(items.size)]  }
+
+    else {
+        0
+    }
+    return result
+}
+fun main() {
+
+    val items = listOf(1, 2, 3, 4, 5)
+println(task1(items))
 }
