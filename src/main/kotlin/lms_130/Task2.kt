ed6f2de1a470b3c2908edc0c22722ab6f18f723d
Bahram Torani <bahram4531@yahoo.com>
2023-03-13 17:25:30 +0100

    Update Task2.kt

--- src/main/kotlin/lms_130/Task2.kt
+++ src/main/kotlin/lms_130/Task2.kt
@@ -1,22 +1,21 @@
 package lms_130
 
 fun task2(items: List<Int>): Int {
+    var result = 0
 
-
-
-    val items = listOf(1, 2, 3, 4)
-
-    val result = if (items.isNotEmpty()) {
-        items.filterIndexed { index, _ -> index % 2 == 0 }
-            .sum()
-    } else {
-        0
+    items.forEachIndexed{index, number ->
+        if (index % 2 == 0) {
+            result += number
+        } else {
+            0
+        }
     }
+
 
     return result
 }
-fun main(){
-    val items = listOf(1, 2, 3, 4)
-    println(task2(items))
 
+fun main () {
+    val result = task2(listOf(1, 2, 3, 4))
+    println(result)
 }
