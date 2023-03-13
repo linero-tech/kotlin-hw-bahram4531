c223012662ea96c18c53d9183b95b16d55481e40
Bahram Torani <bahram4531@yahoo.com>
2023-03-13 19:44:34 +0100

    Update Task3.kt

--- src/main/kotlin/lms_129/Task3.kt
+++ src/main/kotlin/lms_129/Task3.kt
@@ -2,19 +2,12 @@ package lms_129
 
 fun task3(number: Int): Int {
 
-    var number = 5 // example value
     var result = 1
-
     for (i in 1..number) {
         result *= i
     }
-
     return result
 }
 fun main() {
-
-    var number = 5 // example value
-    var result = 1
     println(task3(5))
-    println(task3(1))
 }
