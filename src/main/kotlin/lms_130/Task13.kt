025c578791d9cc9684fa88bfd7dabd7ca32bea10
Bahram Torani <bahram4531@yahoo.com>
2023-03-13 23:52:26 +0100

    Update Task13.kt

--- src/main/kotlin/lms_130/Task13.kt
+++ src/main/kotlin/lms_130/Task13.kt
@@ -1,18 +1,47 @@
 package lms_130
 
 fun task13_1(customers: List<String>): List<String> {
+    val customers = listOf(
+        "mary@tv.com",
+        "john@nas.gov",
+        "john@nas.gov",
+        "ema@post.com",
+        "ema@post.com"
+    )
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val result = customers.toSet().toList()
+
+    return result
 }
 
 fun task13_2(customers: List<String>): List<String> {
+    val customers = listOf(
+        "mary@tv.com",
+        "john@nas.gov",
+        "john@nas.gov",
+        "ema@post.com",
+        "ema@post.com"
+    )
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val result = customers.groupBy { it }
+        .filterValues { it.size > 1 }
+        .keys
+        .toList()
+
+    return result
 }
 
 fun task13_3(customers: List<String>): List<String> {
+    val customers = listOf(
+        "mary@tv.com",
+        "john@nas.gov",
+        "john@nas.gov",
+        "ema@post.com",
+        "ema@post.com"
+    )
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val result = customers.map { it.substringAfter('@').substringBefore('.') }.toSet().toList()
+    return result
 }
 
 fun main() {
