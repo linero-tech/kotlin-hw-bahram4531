d112c8f4f4918c1571ac85f0d2ac0fe7c54a4728
Bahram Torani <bahram4531@yahoo.com>
2023-03-13 21:02:08 +0100

    Update Task10.kt

--- src/main/kotlin/lms_129/Task10.kt
+++ src/main/kotlin/lms_129/Task10.kt
@@ -2,5 +2,17 @@ package lms_129
 
 fun task10(password: String): Boolean {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val regex1 = Regex("[a-z]")
+    val regex2 = Regex("[A-Z]")
+    val regex3 = Regex("[0-9]")
+    val regex4 = Regex("[$#@]")
+    return password.length in 6..10 &&
+            regex1.containsMatchIn(password) &&
+            regex2.containsMatchIn(password) &&
+            regex3.containsMatchIn(password) &&
+            regex4.containsMatchIn(password)
+}
+fun main () {
+    val result = task10("Abcd#3f")
+    println(result)
 }
