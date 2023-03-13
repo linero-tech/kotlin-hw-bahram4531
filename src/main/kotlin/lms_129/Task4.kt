7d7ef021cc8f58b8ce081a8a328e47d294c53d9e
Bahram Torani <bahram4531@yahoo.com>
2023-03-13 19:21:00 +0100

    Update Task4.kt

--- src/main/kotlin/lms_129/Task4.kt
+++ src/main/kotlin/lms_129/Task4.kt
@@ -2,5 +2,17 @@ package lms_129
 
 fun task4(s: String): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = 0
+
+    for (i in 1..1000) {
+        if (i % 9 == 0) {
+            result += i
+        }
+    }
+    return result
+}
+
+fun main() {
+    println(task4("result"))
+
 }
