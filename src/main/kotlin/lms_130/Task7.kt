02d151e7a3a1d65f279af8636a39de43c05b62a0
Bahram Torani <bahram4531@yahoo.com>
2023-03-13 18:34:55 +0100

    Update Task7.kt

--- src/main/kotlin/lms_130/Task7.kt
+++ src/main/kotlin/lms_130/Task7.kt
@@ -1,6 +1,20 @@
 package lms_130
 
 fun task7(items: List<Int>): Int {
+    val items = listOf(10, 5, 11)
+    var result = if (items.isNotEmpty()) items[0] else 0
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    for (i in items) {
+        if (i < result) {
+            result = i
+        }
+    }
+
+    return result
 }
+        fun main() {
+            val items = listOf(10, 5, 11)
+
+        println(task7 (items))
+    }
+
