58b3231efef703f57acfcafbaf80f48d43ba83b4
Bahram Torani <bahram4531@yahoo.com>
2023-03-14 00:02:02 +0100

    Update Task11.kt

--- src/main/kotlin/lms_131/Task11.kt
+++ src/main/kotlin/lms_131/Task11.kt
@@ -2,5 +2,15 @@ package lms_131
 
 fun task11(mapa: Map<Int, String>, prospect: Int): Boolean {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val mapa = mapOf(1 to "A", 2 to "B", 3 to "C")
+    val prospect = 1
+    val result = mapa.containsKey(prospect)
+
+
+    return result
+}
+fun main (){
+    val mapa = mapOf(1 to "A", 2 to "B", 3 to "C")
+
+println(task10(mapa))
 }
