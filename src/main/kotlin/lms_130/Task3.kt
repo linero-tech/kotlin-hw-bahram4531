7b4a36d4f18f2062802bce13948af3177853b968
Bahram Torani <bahram4531@yahoo.com>
2023-03-13 16:07:11 +0100

    Update Task3.kt

--- src/main/kotlin/lms_130/Task3.kt
+++ src/main/kotlin/lms_130/Task3.kt
@@ -1,6 +1,18 @@
 package lms_130
 
-fun task3(items: List<Int>): List<Int> {
+fun task3(items: List<Int>): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val items = listOf(1, 2, 3, 4)
+
+    val result = if (items.isNotEmpty()) {
+        items.filterIndexed { index, _ -> index % 2 == 0 }
+            .sum()
+    } else {
+        0
 }
+    return result
+    }
+fun main(){
+    val items = listOf(1, 2, 3, 4)
+    println(task3(items))
+    }
