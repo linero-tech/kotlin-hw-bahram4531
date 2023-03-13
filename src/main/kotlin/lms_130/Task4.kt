fbb1174a2a59fd2df529e1b8b61777941db8c08a
Bahram Torani <bahram4531@yahoo.com>
2023-03-13 16:36:26 +0100

    Update Task4.kt

--- src/main/kotlin/lms_130/Task4.kt
+++ src/main/kotlin/lms_130/Task4.kt
@@ -2,5 +2,19 @@ package lms_130
 
 fun task4(items: List<Int>, factor: Int): List<Int> {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+
+    val factorOfFactor = mutableSetOf<Int>()
+
+    items.forEach { number ->
+        if (number % factor == 0) {
+            factorOfFactor.add(number)
+        }
+        }
+
+    return factorOfFactor.toList()
 }
+
+fun main() {
+    println(task4(listOf(1, 2, 4, 5, 6, 6, 6), 2))
+}
+
