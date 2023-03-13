b7b88bc3eba5e962f6d421e42cf8db7db52ccacc
Bahram Torani <bahram4531@yahoo.com>
2023-03-13 20:47:48 +0100

    Update Task9.kt

--- src/main/kotlin/lms_129/Task9.kt
+++ src/main/kotlin/lms_129/Task9.kt
@@ -1,6 +1,17 @@
 package lms_129
 
 fun task9(temperature: String): String {
+    val magnitude = temperature.dropLast(1).toDouble()
+    val unit = temperature.last().uppercase()
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val convertedTemp = if (unit == "F") {
+        ((magnitude - 32) * 5 / 9).toInt()
+    } else {
+        (magnitude * 9 / 5 + 32).toInt()
+    }
+    val convertedUnit = if (unit == "C") "F" else "C"
+    return "$convertedTemp$convertedUnit"
+}
+fun main() {
+    println(task9("50f"))
 }
