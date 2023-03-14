a8fa0b62ca63ca8b01bfa86b2238a7d82e086a01
Bahram Torani <bahram4531@yahoo.com>
2023-03-14 15:19:35 +0100

    Update Task15.kt

--- src/main/kotlin/lms_131/Task15.kt
+++ src/main/kotlin/lms_131/Task15.kt
@@ -2,20 +2,17 @@ package lms_131
 
 fun task15(sentence: String): Map<Char, Int> {
 
-    val words = sentence.toLowerCase().split("[ ,.;:]".toRegex()).filter { it.isNotBlank() }
     val result = mutableMapOf<Char, Int>()
-    for (word in words) {
-        val firstLetter = word.firstOrNull()
-        if (firstLetter != null && firstLetter.isLetter()) {
+
+    for (word in sentence.split(" ")) {
+        val firstLetter = word.firstOrNull { it.isLetter() }?.toLowerCase()
+        if (firstLetter != null) {
             result[firstLetter] = result.getOrDefault(firstLetter, 0) + 1
         }
     }
+        return result}
 
-    return result
-}
-
-
-fun main(){
+fun main (){
     val sentence = "This is the Very small Text"
-    println(task15(sentence))
+println(task15(sentence))
 }
