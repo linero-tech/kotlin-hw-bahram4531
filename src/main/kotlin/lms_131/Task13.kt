3075f19f3a393097ce5301c4f6ae172ef66dddef
Bahram Torani <bahram4531@yahoo.com>
2023-03-14 12:18:54 +0100

    Update Task13.kt

--- src/main/kotlin/lms_131/Task13.kt
+++ src/main/kotlin/lms_131/Task13.kt
@@ -1,6 +1,25 @@
 package lms_131
 
-fun task13(sentence: String): String {
+import javax.print.DocFlavor.STRING
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+fun task13(sentence: String): String {
+    val words = sentence.toLowerCase().split("[ ,.;:]".toRegex()).filter { it.isNotBlank() }
+    val frequencyMap = mutableMapOf<String, Int>()
+    for (word in words) {
+        frequencyMap[word] = frequencyMap.getOrDefault(word, 0) + 1}
+    var result = ""
+    var maxFrequency = 0
+    for ((word, frequency) in frequencyMap) {
+        if (frequency > maxFrequency) {
+            result = word
+            maxFrequency = frequency
+        }
+    }
+
+    return result
+}
+
+fun main() {
+    val sentence = "This THIS is still the very very same"
+    println(task13(sentence))
 }
