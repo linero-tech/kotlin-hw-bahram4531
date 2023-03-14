082d0f362fac1b385a34b0fec2044c0d40a78753
Bahram Torani <bahram4531@yahoo.com>
2023-03-14 16:54:57 +0100

    Update Task9.kt

--- src/main/kotlin/lms_131/Task9.kt
+++ src/main/kotlin/lms_131/Task9.kt
@@ -1,5 +1,7 @@
 package lms_131
 
+import lms_130.result
+
 fun task9(grades: Map<String, List<Double>>): Double {
     var totalGrades = 0.0
     var count = 0
@@ -18,8 +20,11 @@ fun task9(grades: Map<String, List<Double>>): Double {
     return result
 }
 fun main(){
-    val grades = mapOf("Ana" to listOf(4.0, 4.5, 5.0),
+    val grades = mapOf(
+        "Ana" to listOf(4.0, 4.5, 5.0),
         "John" to listOf(5.0, 5.0, 3.0),
         "Lise" to listOf(5.0, 5.0, 5.0))
-println(task9(grades))
+println(task9(grades,))
+    println("Average grade: $result")
+
 }
