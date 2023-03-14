095942a3b1293924dcb6bc140d51681f57a2c89c
Bahram Torani <bahram4531@yahoo.com>
2023-03-14 13:20:36 +0100

    Update Task15.kt

--- src/main/kotlin/lms_131/Task15.kt
+++ src/main/kotlin/lms_131/Task15.kt
@@ -2,5 +2,20 @@ package lms_131
 
 fun task15(sentence: String): Map<Char, Int> {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val words = sentence.toLowerCase().split("[ ,.;:]".toRegex()).filter { it.isNotBlank() }
+    val result = mutableMapOf<Char, Int>()
+    for (word in words) {
+        val firstLetter = word.firstOrNull()
+        if (firstLetter != null && firstLetter.isLetter()) {
+            result[firstLetter] = result.getOrDefault(firstLetter, 0) + 1
+        }
+    }
+
+    return result
+}
+
+
+fun main(){
+    val sentence = "This is the Very small Text"
+    println(task15(sentence))
 }
