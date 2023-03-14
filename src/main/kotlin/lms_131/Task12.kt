58b48a534730ccb69754d17b70d498f1862e2d06
Bahram Torani <bahram4531@yahoo.com>
2023-03-14 02:38:23 +0100

    Update Task12.kt

--- src/main/kotlin/lms_131/Task12.kt
+++ src/main/kotlin/lms_131/Task12.kt
@@ -1,6 +1,22 @@
 package lms_131
 
+import lms_130.result
+
+
 fun task12(a: List<Int>, b: List<String>): Map<Int, String> {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val result = mutableMapOf<Int, String>()
+
+    for (i in a.indices) {
+        result[a[i]] = b[i]
+    }
+
+
+    return result
+}
+
+fun main() {
+    val a = listOf(1, 2, 3, 4)
+    val b = listOf("W", "X", "Y", "Z")
+    println(task12(a,b))
 }
