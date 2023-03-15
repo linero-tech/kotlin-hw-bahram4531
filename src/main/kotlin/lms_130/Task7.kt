3268073650876350d7c20ae81e851139503319c5
Bahram Torani <bahram4531@yahoo.com>
2023-03-15 02:20:21 +0100

    Update Task7.kt

--- src/main/kotlin/lms_130/Task7.kt
+++ src/main/kotlin/lms_130/Task7.kt
@@ -1,9 +1,7 @@
 package lms_130
 
 fun task7(items: List<Int>): Int {
-    return if (items.isEmpty())
-    {
-        0
+    return if (items.isEmpty()) { 0
     } else {
         var smallestValue = items.first()
         items.forEach { number ->
