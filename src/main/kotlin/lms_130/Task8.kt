094709ea69c7c5b51156796c73e856defcce58c4
Bahram Torani <bahram4531@yahoo.com>
2023-03-15 02:22:37 +0100

    Update Task8.kt

--- src/main/kotlin/lms_130/Task8.kt
+++ src/main/kotlin/lms_130/Task8.kt
@@ -1,16 +1,15 @@
 package lms_130
 fun task8(list: List<Int>): List<Int>
 {
-    val top4 = HashSet<Int>()
-
-    for (item in list) {
-        top4.add(item)
-        while (top4.size > 4) {
-            top4.remove(top4.minOrNull())
+    val topThree = HashSet<Int>()
+    for (item in list)
+    {
+        topThree.add(item)
+        while (topThree.size > 3) {
+            topThree.remove(topThree.minOrNull())
         }
     }
-
-    return top4.toList().sorted()
+    return topThree.toList().sorted()
 }
 fun main() {
     println(task8(listOf(1, 2, 2)))
