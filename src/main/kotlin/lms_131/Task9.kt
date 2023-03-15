515102007f6804472d9c6247a8b1571947c9a040
Bahram Torani <bahram4531@yahoo.com>
2023-03-15 02:00:42 +0100

    Update Task9.kt

--- src/main/kotlin/lms_131/Task9.kt
+++ src/main/kotlin/lms_131/Task9.kt
@@ -3,28 +3,27 @@ package lms_131
 import lms_130.result
 
 fun task9(grades: Map<String, List<Double>>): Double {
-    var totalGrades = 0.0
+    var sum = 0.0
     var count = 0
 
-    for (studentGrades in grades.values) {
+    for ((_, studentGrades) in grades) {
         for (grade in studentGrades) {
-            totalGrades += grade
+            sum += grade
             count++
         }
     }
 
-    val average = totalGrades / count
-
-    val result = String.format("%.2f", average).toDouble() // round to 2 decimal places
+    val result = if (count > 0) sum / count else 0.0
 
     return result
 }
-fun main(){
-    val grades = mapOf(
-        "Ana" to listOf(4.0, 4.5, 5.0),
-        "John" to listOf(5.0, 5.0, 3.0),
-        "Lise" to listOf(5.0, 5.0, 5.0))
-println(task9(grades,))
-    println("Average grade: $result")
 
-}
+    fun main() {
+        val grades = mapOf(
+            "Ana" to listOf(4.0, 4.5, 5.0),
+            "John" to listOf(5.0, 5.0, 3.0),
+            "Lise" to listOf(5.0, 5.0, 5.0)
+        )
+        println(task9(grades,))
+
+    }
